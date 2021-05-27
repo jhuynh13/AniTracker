70816f87c1458274320c518c5ad9fbcc32a77191
Johnny Huynh <40502720+jhuynh13@users.noreply.github.com>
2021-05-26 21:15:02 -0400

    Update settings.py

--- AniTracker/AniTracker/settings.py
+++ AniTracker/AniTracker/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sd)4!sw(-cz3s97lm3echr8*gy30j!2(#u9=pcungve8-^2wsu'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
